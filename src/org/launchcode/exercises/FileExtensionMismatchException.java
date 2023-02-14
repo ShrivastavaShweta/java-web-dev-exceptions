d6169340760b481e8c7ccd652f4b4706512ddbb5
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-02-14 15:01:38 -0600

    finished part1 of the exercise

---
+++ src/org/launchcode/exercises/FileExtensionMismatchException.java
@@ -0,0 +1,7 @@
+package org.launchcode.exercises;
+
+public class FileExtensionMismatchException extends Exception{
+    public FileExtensionMismatchException(String message){
+        super(message);
+    }
+}
--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -1,13 +1,22 @@
 package org.launchcode.exercises;
 
+
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
 
     public static void main(String[] args) {
 
         // Test out your Divide() function!
+        try {
+            System.out.println(Divide(8, 4));
+            System.out.println(Divide(8, 0));
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+        }
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,18 +24,45 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
+
         // Test out your CheckFileExtension() function!
+            for (String key: studentFiles.keySet()){
+                try {
+                    int score = CheckFileExtension(studentFiles.get(key));
+                    System.out.println("" + score);
+                } catch (FileExtensionMismatchException e){
+                    e.printStackTrace();
+                }
+            }
     }
 
-    public static void Divide(int x, int y)
-    {
+    public static double Divide(int x, int y) {
         // Write code here!
+        try {
+            return x/y;
+        } catch (ArithmeticException e){
+            throw new ArithmeticException("Divide by zero");
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
+    public static int CheckFileExtension(String fileName) throws FileExtensionMismatchException {
         // Write code here!
-        return 0;
+        String extension = "";
+        int points = 0;
+
+        int i = fileName.lastIndexOf('.');
+        if (i > 0){
+            extension = fileName.substring(i+1);
+        }
+
+        if (extension.equals("")|| extension.equals(null)){
+            points += -1;
+            throw new FileExtensionMismatchException("File extension not found");
+        } else if (extension.equals("java")) {
+            points += 1;
+        }
+
+        return points;
     }
 
 }
